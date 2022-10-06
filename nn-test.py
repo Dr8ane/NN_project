a9a12731b4919240e463a4d82580f970cadb3b6f
Dr8ane <99963376+Dr8ane@users.noreply.github.com>
2022-10-06 22:41:27 +0300

    Update nn-test.py

--- nn-test.py
+++ nn-test.py
@@ -1,22 +1,24 @@
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.python.keras.layers import Activation, Dropout, Flatten, Dense
+import scipy
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
 # Каталог с данными для обучения
-train_dir = 'train'
+train_dir = 'images/for NN/train'
 # Каталог с данными для проверки
-val_dir = 'val'
+val_dir = 'images/for NN/val'
 # Каталог с данными для тестирования
-test_dir = 'test'
+test_dir = 'images/for NN/test'
 # Размеры изображения
 img_width, img_height = 150, 150
 # Размерность тензора на основе изображения для входных данных в нейронную сеть
 # backend Tensorflow, channels_last
 input_shape = (img_width, img_height, 3)
 # Количество эпох
-epochs = 30
+epochs = 10
 # Размер мини-выборки
-batch_size = 16
+batch_size = 20
 # Количество изображений для обучения
 nb_train_samples = 17500
 # Количество изображений для проверки
@@ -40,9 +42,9 @@ model.add(Flatten())
 model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-model.compile(loss='binary_crossentropy',
+model.add(Dense(3))
+model.add(Activation('softmax'))
+model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 datagen = ImageDataGenerator(rescale=1. / 255)
@@ -50,18 +52,18 @@ train_generator = datagen.flow_from_directory(
     train_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical')
 val_generator = datagen.flow_from_directory(
     val_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical')
 test_generator = datagen.flow_from_directory(
     test_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
-    class_mode='binary')
-model.fit_generator(
+    class_mode='categorical')
+model.fit(
     train_generator,
     steps_per_epoch=nb_train_samples // batch_size,
     epochs=epochs,
